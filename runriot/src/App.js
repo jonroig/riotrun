832cf590c381eb77663b6360345065930170b565
Jon Roig <jon@jonroig.com>
2018-06-17 05:06:14 -0700

    better build

--- runriot/src/App.js
+++ runriot/src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Riot.run</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Something really interesting will happen here...
         </p>
       </div>
     );
